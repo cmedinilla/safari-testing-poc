5c29c3cae84c06a39790148b713660736d63966f
Medinilla <cmedinilla@apexsystems.com>
2024-07-15 16:30:00 -0600

    Run test on macos hosted and GH actions ubuntu latest (#1)
    * Run test on macos hosted and GH actions ubuntu latest
    
    * Add EOF return

--- test.js
+++ test.js
@@ -4,6 +4,6 @@ fixture `Test structure`
   .page `https://demo-ready-alexandrite.azurewebsites.net`;
 
 test('Test1', async t => {
-  const title = Selector('a').withText('Alexandrite');
+  const title = Selector('a').withText('ALEXANDRITE');
   await t.expect(title.exists).ok();
 });
